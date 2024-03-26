00ec68a19626e0335c0f95b05cb5a123b2ba81ab
barare <blue90915@mail.ir>
2024-03-26 23:38:51 +0330

    crossterm, chrono

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use std::{thread::sleep,
+use crossterm::{terminal::{ClearType,EnterAlternateScreen,LeaveAlternateScreen,enable_raw_mode,disable_raw_mode,Clear},
+    execute,};
+
+use std::{io::{Result,stdout},
+    thread::sleep,
     time::Duration};
-fn main() {
-    let mut target_time =16*60*60;
-    let mut now =0;
-    loop{
-           println!(" remaining: {:?}   elapsed: {:?}",target_time,now);
-           sleep(Duration::from_secs(1));
-           now +=1;
-           target_time -=1;
-           if now == target_time{
-            break;
-           }
-    }
+use chrono::{TimeDelta,Local};
+fn main() ->Result<()>{
+    enable_raw_mode()?;
+    let mut stdout=stdout();
+    // let terminal =Terminal::new();
+execute!(stdout,EnterAlternateScreen)?;
+
+let mut now =Local::now();
+let target_time =now+ TimeDelta::try_seconds(6).unwrap();
+while now < target_time{
+    println!(" target time: {:?}   now : {:?}",target_time,now);
+    now =now + TimeDelta::try_seconds(1).unwrap();
+    //    target_time =target_time- TimeDelta::try_seconds(1).unwrap();
+    sleep(Duration::from_secs(1));
+    Clear(ClearType::All);
+}
+disable_raw_mode()?;
+execute!(stdout,LeaveAlternateScreen)?;
+Ok(())
 }
 // }
